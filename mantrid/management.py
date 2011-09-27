308d938185d5f2ab460eb82a6f78ce8c3016fc0f
Andrew Godwin <andrew@aeracode.org>
2011-09-27 14:30:38 +0100

    Get/set for hosts

--- mantrid/management.py
+++ mantrid/management.py
@@ -1,4 +1,5 @@
 import json
+import re
 
 
 class HttpNotFound(Exception):
@@ -11,12 +12,19 @@ class HttpMethodNotAllowed(Exception):
     pass
 
 
+class HttpBadRequest(Exception):
+    "Exception raised for an invalidly formed host entry."
+    pass
+
+
 class ManagementApp(object):
     """
     Management WSGI app for the Mantrid loadbalancer.
     Allows endpoints to be changed via HTTP requests to
     the management port.
     """
+
+    host_regex = re.compile(r"^/host/([^/]+)/?$")
 
     def __init__(self, balancer):
         self.balancer = balancer
@@ -39,8 +47,12 @@ class ManagementApp(object):
             start_response('405 Method Not Allowed', [('Content-Type', 'application/json')])
             return [json.dumps({"error": "method_not_allowed"})]
         # Dispatch to the named method
+        body = environ['wsgi.input'].read()
+        if body:
+            body = json.loads(body)
         response = handler(
             environ['PATH_INFO'].lower(),
+            body,
         )
         # Send the response
         start_response('200 OK', [('Content-Type', 'application/json')])
@@ -55,10 +67,64 @@ class ManagementApp(object):
                 return self.set_all
             else:
                 raise HttpMethodNotAllowed()
+        elif self.host_regex.match(path):
+            if method == "get":
+                return self.get_single
+            if method == "put":
+                return self.set_single
+            else:
+                raise HttpMethodNotAllowed()
         else:
             raise HttpNotFound()
 
     ### Handling methods ###
 
-    def get_all(self, path):
+    def host_errors(self, hostname, details):
+        """
+        Validates the format of a host entry
+        Returns an error string, or None if it is valid.
+        """
+        if not hostname or not isinstance(hostname, basestring):
+            return "hostname_invalid"
+        if not isinstance(details, list):
+            return "host_details_not_list"
+        if len(details) != 3:
+            return "host_details_wrong_length"
+        if not details[0] not in self.balancer.action_mapping:
+            return "host_action_invalid"
+        if not isinstance(details[1], dict):
+            return "host_kwargs_not_dict"
+        if not isinstance(details[2], bool):
+            return "host_match_subdomains_not_bool"
+        return None
+
+    def get_all(self, path, body):
         return self.balancer.hosts
+
+    def set_all(self, path, body):
+        "Replaces the hosts list with the provided input"
+        # Do some error checking
+        if not isinstance(body, dict):
+            raise HttpBadRequest("body_not_a_dict")
+        for hostname, details in body.items():
+            error = self.host_errors(hostname, details)
+            if error:
+                raise HttpBadRequest("%s:%s" % (hostname, error))
+        # Replace
+        self.balancer.hosts = body
+        return {"ok": True}
+
+    def get_single(self, path, body):
+        host = self.host_regex.match(path).group(1)
+        if host in self.balancer.hosts:
+            return self.balancer.hosts[host]
+        else:
+            return None
+
+    def set_single(self, path, body):
+        host = self.host_regex.match(path).group(1)
+        error = self.host_errors(host, body)
+        if error:
+            raise HttpBadRequest("%s:%s" % (host, error))
+        self.balancer.hosts[host] = body
+        return {"ok": True}
