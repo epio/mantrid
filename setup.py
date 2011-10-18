fe67f7f061bd0cebdfe2a5a89eae0644b8533738
Andrew Godwin <andrew@aeracode.org>
2011-10-18 16:25:54 +0100

    More setup polishing

--- setup.py
+++ setup.py
@@ -11,9 +11,11 @@ from mantrid import __version__
 setup(
     name = 'mantrid',
     version = __version__,
+    author = "Epio Limited",
+    author_email= "team@ep.io",
     description = 'A pure-Python loadbalancer.',
     packages = find_packages("."),
-    classifiers=[
+    classifiers = [
         'Development Status :: 5 - Production/Stable',
         'License :: OSI Approved :: BSD License',
         'Programming Language :: Python',
@@ -23,4 +25,11 @@ setup(
         mantrid = mantrid.loadbalancer:Balancer.main
         mantrid-client = mantrid.cli:MantridCli.main
     """,
+    package_data = {
+        "mantrid": ["errors/*.http"],
+    },
+    requires = [
+        "argparse",
+        "eventlet (>=0.9.16)",
+    ],
 )
